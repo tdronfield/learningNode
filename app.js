b4080a3b436a52f343e754cc9c17e999cf2d69b2
Dronfield <t_dronfield@ats-hr420-dm28.fc.ca>
2019-09-23 19:30:29 -0400

    using express

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const http = require('http');
+const express = require('express');
+const path = require('path'); 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// heroku assigns a port when it deploys via the process (environment variables - coming soon)
+// locally this will run at port 3000; remotely it'll run wherever heroku tells it to run
+const port = process.env.PORT || 3000; // a double pipe || means "or"
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const app = express();
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(express.static('public'));
+
+// a forward slash is the home route (i.e index.html)
+app.get('/', (req, res) => {
+  //res.send('Hello World!');
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+  // this builds localhost:3000/views/index.html
+})
+
+app.get('/contact', (req, res) => {
+  //res.send('on the contact page!');
+  res.sendFile(path.join(__dirname + '/views/contact.html'));
+})
+
+app.listen(port,() => {
+  console.log(`Server running at ${port}`);
+})
