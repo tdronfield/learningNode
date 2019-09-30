9cbdad5611e60986e84f74dd6ea4a074cc7f5ac0
Dronfield <t_dronfield@ats-hr420-dm27.fc.ca>
2019-09-30 19:47:33 -0400

    using handlebars and dynamic data

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    uname: 'root',
+    upass: 'root', //windows users leave blank
+    port: 8889, //windows 3306
+    host: 'localhost',
+    db: 'bizcard'
+}
+
+//make this a public file our app can read
+module.exports = config;
